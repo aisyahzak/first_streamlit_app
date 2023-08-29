0ce1091f3f07af54e6c0fbdf3763e5f944b7f136
aisyahzak <aisyahzak1995@gmail.com>
2023-08-29 14:54:56 +0800

    Update streamlit_app.py
    Corrected a typo and removed a line about json text

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,10 +24,9 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruitvice api response
 import requests
-fruitvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 streamlit.header('Fruityvice Fruit Advice!')
-streamlit.text(fruityvice_response.json())
 
 # Normalizing the text 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
